6ef3fe56f65bc0784c68d7feebb731a3f21269fa
Jones Vital Denicoli <jones.vital@consultores.rnp.br>
2024-09-26 10:03:15 -0300

    Inicio do processo de criação de giftcard

--- src/main/java/com/giftflow/giftflow_backend/repositories/GiftcardRepository.java
+++ src/main/java/com/giftflow/giftflow_backend/repositories/GiftcardRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.giftflow.giftflow_backend.entities.Giftcard;
 
 @Repository
-interface GiftcardRepository extends JpaRepository<Giftcard, Integer>{
+public interface GiftcardRepository extends JpaRepository<Giftcard, Integer>{
     
 }
